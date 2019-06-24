6bb8e55ad148128186fe708672bda4cb43fe8d56
Brayan Sanchez Torres - Ceiba Software <brayan.sanchez@ceiba.com.co>
2019-06-24 08:09:40 -0500

    version 0.5

--- src/test/java/com/ceiba/parquedero/dominio/servicio/ServicioActualizarRegistroTest.java
+++ src/test/java/com/ceiba/parquedero/dominio/servicio/ServicioActualizarRegistroTest.java
@@ -1,15 +1,23 @@
 package com.ceiba.parquedero.dominio.servicio;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.ceiba.parquedero.dominio.excepcion.ParqueaderoExcepcion;
 import com.ceiba.parquedero.dominio.repositorio.RepositorioParquedero;
 import com.ceiba.parquedero.dominio.repositorio.RepositorioRegistro;
 
 public class ServicioActualizarRegistroTest {
+	
 	@InjectMocks
+	private ServicioActualizarRegistro servicioActualizarRegistro;
+	
+	@Mock
 	private ServicioParqueadero servicioParqueadero;
 
 	@Mock
@@ -21,6 +29,20 @@ public class ServicioActualizarRegistroTest {
 	@Before
 	public void init() {
 		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test(expected = ParqueaderoExcepcion.class)
+	public void validarExistenciaEnParqueaderoCarro() throws ParqueaderoExcepcion {
+		when(servicioParqueadero.existeEnParqueadero("CARRO")).thenReturn(false);
+
+		this.servicioActualizarRegistro.validarExistenciaEnParqueadero("CARRO");
+	}
+	
+	@Test(expected = ParqueaderoExcepcion.class)
+	public void validarExistenciaEnParqueaderoMoto() throws ParqueaderoExcepcion {
+		when(servicioParqueadero.existeEnParqueadero("MOTO")).thenReturn(false);
+
+		this.servicioActualizarRegistro.validarExistenciaEnParqueadero("MOTO");
 	}
 	
 	
