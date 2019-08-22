ad650064b6eed3ae06f32106c17c31629caf711a
brayanCeiba <brayan.sanchez@ceiba.com.co>
2019-08-22 07:41:22 -0500

    Prueba

--- src/main/java/com/ceiba/parquedero/ParquederoCeibaApplication.java
+++ src/main/java/com/ceiba/parquedero/ParquederoCeibaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ParquederoCeibaApplication {
-
+	//comentario
 	public static void main(String[] args) {
 		SpringApplication.run(ParquederoCeibaApplication.class, args);
 	}
